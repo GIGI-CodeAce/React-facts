8a881411320f6f9b52ded50b7f90e2a2da48b3ee
Dobre Robert <dobrerobert441@gmail.com>
2024-08-28 13:22:58 +0300

    fixes

--- src/nav.jsx
+++ src/nav.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import './nav.css'
 
 export default function NavBar() {
-    return (
-            <header>
-          <img id='react' src='./Media/reactLogo.png' alt="alt" />
-          <h3>React facts</h3>
-          <nav>
+  return (
+      <header>
+<img id='react' src='https://raw.githubusercontent.com/GIGIsOtherStuff/reactFacts/main/Media/reactLogo.png' alt="React Logo" />
+        <h3>React facts</h3>
+        <nav>
             <li><a href="#" target="_blank"></a>Home</li>
             <li><a href="#" target="_blank"></a>Contact</li>
             <li><a href="#" target="_blank"></a>Info</li>
             <li><a href="#" target="_blank"></a>About us</li>
-          </nav>
-        </header>
+        </nav>
+      </header>
   )
-  }
+}
